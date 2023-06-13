ea10e4072e8c5bd63426a22c9a3e0a3fcc5463eb
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-06-13 21:28:58 +0300

    feature: set react environment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header/Header';
+import Container from './components/Container/Container';
+
+function App() {
+    return (
+        <Container>
+            <Header />
+        </Container>
+    );
+}
+
+export default App;
